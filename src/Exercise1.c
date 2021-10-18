b12a8644370c251c2c9b65dda6a25ee518dd4452
ititiu20312quocthinh <91869213+ititiu20312quocthinh@users.noreply.github.com>
2021-10-18 14:32:36 +0700

    exercise1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -14,9 +14,17 @@ Ex:
 
 void Ex1(int arr[], int n){
 	//Your codes here
-    
+    int i, j;
+	int sym = 1;
+	
+	for (i = 0, j = n - 1; (i <= floor((n-1) / 2)) && (j >= ceil((n - 1) / 2)); i++, j--){
+ 
+    if (sym == 0)
+	     printf("asymmetric");
+	else
+	     printf("symmetric");
 }
-
+}
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
